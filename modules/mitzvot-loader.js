dd81949860e231a3b0890420f5eaa234cebc8bdb
Northwestchefs <165867661+Northwestchefs@users.noreply.github.com>
2026-03-14 05:44:58 -0700

    Add mitzvot 351-400 and bump cache version

--- modules/mitzvot-loader.js
+++ modules/mitzvot-loader.js
@@ -1,5 +1,5 @@
-const LOCAL_STORAGE_KEY = 'sacred.mitzvot.cache.v5';
-const MIN_EXPECTED_MITZVOT = 350;
+const LOCAL_STORAGE_KEY = 'sacred.mitzvot.cache.v6';
+const MIN_EXPECTED_MITZVOT = 400;
 let mitzvotCache = null;
 
 export async function loadMitzvot() {
